dd4da873bdc836fdd05be2b9f2c2707bbab6f5ed
jdavidIP <ibapol2003@gmail.com>
2024-03-15 22:48:17 -0400

    Commit #3 - Function to calculate fuel consumption added

--- Server/server.cpp
+++ Server/server.cpp
@@ -16,9 +16,21 @@ void handleClient(SOCKET clientSocket) {
             std::cout << "Received from client:" << std::endl;
             receivedPacket.Display(std::cout);
 
+            // TO DO: Implement a way to use "calculateFuelConsumption" to do just that.
+
             // You can store the received packet in a vector or any other data structure as needed
         }
     } while (bytesReceived > 0);
 
     closesocket(clientSocket);
+}
+
+float calculateFuelConsumption(char* previousFuel, char* currentFuel, float elapsedTime)
+{
+    float previousFuelLevel = atof(previousFuel);
+    float currentFuelLevel = atof(currentFuel);
+
+    float fuelConsumptionRate = (previousFuelLevel - currentFuelLevel) / elapsedTime;
+
+    return fuelConsumptionRate;
 }
